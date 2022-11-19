f15b3d71486812b4ff15696a46062805b7567358
JosephSimonPrudente <prudentejoseph3@gmail.com>
2022-11-19 16:53:25 +0800

    printing all the required data to see result

--- typing speed Test.py
+++ typing speed Test.py
@@ -41,7 +41,7 @@ def elapsedtime(stime, etime):
 
 # this was the paragraph which you have you have to type to check your speed
 if __name__ == '__main__':
-    prompt = "Generating random paragraphs can be an excellent way for writers to get their creative flow going at the beginning of the day. The writer has no idea what topic the random paragraph will be about when it appears. This forces the writer to use creativity to complete one of three common writing challenges. The writer can use the paragraph as the first one of a short story and build upon it. A second option is to use the random paragraph somewhere in a short story they create. The third option is to have the random paragraph be the ending paragraph in a short story. No matter which of these challenges is undertaken, the writer is forced to use creativity to incorporate the paragraph into their writing."
+    prompt = "We may easily improve our writing skills by using a random paragraph generator. One of the best way to develop writing skills and English is by writing or reading. This tool is simple to use and you can use it anywhere anytime."
     print("Type this:-",prompt)
 
 #Checking to input   
@@ -58,5 +58,10 @@ if __name__ == '__main__':
     time = round(elapsedtime(stime,etime),2)
     speed = speed(inprompt,stime,etime)
     errors = tperror(prompt)
-    
+
+# printing all the required data to see result
+    print("total time elapsed:", time ,"Seconds")
+    print("Your average typing speed was ", speed , "words per minute (w/m)")
+    print("with the total of ", errors, "errors")
+
 
