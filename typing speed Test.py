74b5ef3838977bced49cfb9594e34211749be8cb
JosephSimonPrudente <prudentejoseph3@gmail.com>
2022-11-19 16:35:29 +0800

    this was the paragraph which you have you have to type to check your speed

--- typing speed Test.py
+++ typing speed Test.py
@@ -39,5 +39,9 @@ def elapsedtiem(stime, etime):
     time = etime - stime # etime is the end time and stime is the start time 
     return time 
 
-
+# this was the paragraph which you have you have to type to check your speed
+if __name__ == '__main__':
+    prompt = "Generating random paragraphs can be an excellent way for writers to get their creative flow going at the beginning of the day. The writer has no idea what topic the random paragraph will be about when it appears. This forces the writer to use creativity to complete one of three common writing challenges. The writer can use the paragraph as the first one of a short story and build upon it. A second option is to use the random paragraph somewhere in a short story they create. The third option is to have the random paragraph be the ending paragraph in a short story. No matter which of these challenges is undertaken, the writer is forced to use creativity to incorporate the paragraph into their writing."
+    print("Type this:-",prompt)
+    
 
