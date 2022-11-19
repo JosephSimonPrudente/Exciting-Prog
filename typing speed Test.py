ea5bc926363d9a21328a97a031e7fd6e173980f4
JosephSimonPrudente <prudentejoseph3@gmail.com>
2022-11-19 17:23:14 +0800

    adding the youtube linkto comment

--- typing speed Test.py
+++ typing speed Test.py
@@ -70,4 +70,5 @@ if __name__ == '__main__':
     print("=================================================================")
     print("with the total of ", errors, "errors")
 
+#https://www.youtube.com/watch?v=AkKFLes-_VI&list=PLpp8-k7G_6Y3Wj1suZQ-9lATFzFuGw93x&index=8
 
